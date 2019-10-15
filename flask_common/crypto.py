705f0d625b323b25080966254113ca1a4698620f
João Sampaio <joao.sampaio@close.com>
2019-10-15 09:15:40 -0300

    Rename variables

--- flask_common/crypto.py
+++ flask_common/crypto.py
@@ -4,12 +4,12 @@ from Crypto.Util import Counter
 import hashlib
 import hmac
 
-AES_BLOCK_SIZE = 32  # 256 bit
-IV_SIZE = 16
-HMAC_KEY_SIZE = 32
+AES_KEY_SIZE = 32  # bytes
+HMAC_KEY_SIZE = 32  # bytes
+IV_SIZE = 16  # bytes
 HMAC_DIGEST = hashlib.sha256
 HMAC_DIGEST_SIZE = hashlib.sha256().digest_size
-KEY_LENGTH = AES_BLOCK_SIZE + HMAC_KEY_SIZE
+KEY_LENGTH = AES_KEY_SIZE + HMAC_KEY_SIZE
 
 V2_MARKER = b'v2'
 
@@ -48,8 +48,8 @@ def aes_decrypt(key, data, force_v1=False):
         data = data[IV_SIZE:]
         is_v2 = True
     else:
-        iv = data[:AES_BLOCK_SIZE]
-        data = data[AES_BLOCK_SIZE:]
+        iv = data[:AES_KEY_SIZE]
+        data = data[AES_KEY_SIZE:]
         is_v2 = False
     return aes_decrypt_iv(key, data, iv, is_v2)
 
@@ -57,8 +57,8 @@ def aes_decrypt(key, data, force_v1=False):
 # Encrypt + sign using no IV or provided IV. Pass empty string for no IV.
 # Note: You should normally use aes_encrypt()
 def aes_encrypt_iv(key, data, iv):
-    aes_key = key[:AES_BLOCK_SIZE]
-    hmac_key = key[AES_BLOCK_SIZE:]
+    aes_key = key[:AES_KEY_SIZE]
+    hmac_key = key[AES_KEY_SIZE:]
     initial_value = long(iv.encode("hex"), 16) if iv else 1
     ctr = Counter.new(128, initial_value=initial_value)
     cipher = AES.new(aes_key, AES.MODE_CTR, counter=ctr).encrypt(data)
@@ -69,8 +69,8 @@ def aes_encrypt_iv(key, data, iv):
 # Verify + decrypt using no IV or provided IV. Pass empty string for no IV.
 # Note: You should normally use aes_decrypt()
 def aes_decrypt_iv(key, data, iv, is_v2):
-    aes_key = key[:AES_BLOCK_SIZE]
-    hmac_key = key[AES_BLOCK_SIZE:]
+    aes_key = key[:AES_KEY_SIZE]
+    hmac_key = key[AES_KEY_SIZE:]
     sig_size = HMAC_DIGEST_SIZE
     cipher = data[:-sig_size]
     sig = data[-sig_size:]
