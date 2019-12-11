248bfb1b46d3b1b09e436434c1c1c3b20996a868
João Sampaio <joao.sampaio@close.com>
2019-12-11 17:12:03 -0300

    Add EncryptedBinaryField

--- flask_common/mongo/fields/crypto.py
+++ flask_common/mongo/fields/crypto.py
@@ -4,26 +4,27 @@ from mongoengine.fields import BinaryField
 from flask_common.crypto import (
     KEY_LENGTH,
     AuthenticationError,
-    aes_encrypt,
     aes_decrypt,
+    aes_encrypt,
 )
 
 
-class EncryptedStringField(BinaryField):
+class EncryptedBinaryField(BinaryField):
     """
-    Encrypted string field. Uses AES256 bit encryption with a different 128 bit
-    IV every time the field is saved. Encryption is completely transparent to
-    the user as the field automatically unencrypts when the field is accessed
-    and encrypts when the document is saved.
+    Encrypted binary data field. Encryption is completely transparent
+    to the caller as the field automatically decrypts when the field
+    is accessed and encrypts when the document is saved. The
+    underlying algorithm currently is AES-256.
     """
 
     def __init__(self, key_or_list, *args, **kwargs):
         """
-        key_or_list: 64 byte binary string containing a 256 bit AES key and a
-        256 bit HMAC-SHA256 key.
-        Alternatively, a list of keys for decryption may be provided. The
-        first key will always be used for encryption. This is e.g. useful for
-        key migration.
+        key_or_list: A 512-bit binary string containing a 256-bit AES
+        key followed by a 256-bit HMAC-SHA256 key.
+        Alternatively, a list of keys for decryption may be provided.
+        The first key will always be used for encryption, the other
+        ones will be sequentially tried for decryption. This is e.g.
+        useful for key migration.
         """
         if isinstance(key_or_list, (list, tuple)):
             self.key_list = key_or_list
@@ -32,7 +33,7 @@ class EncryptedStringField(BinaryField):
         assert len(self.key_list) > 0, "No key provided"
         for key in self.key_list:
             assert len(key) == KEY_LENGTH, 'invalid key size'
-        return super(EncryptedStringField, self).__init__(*args, **kwargs)
+        super(EncryptedBinaryField, self).__init__(*args, **kwargs)
 
     def _encrypt(self, data):
         return Binary(aes_encrypt(self.key_list[0], data))
@@ -51,3 +52,18 @@ class EncryptedStringField(BinaryField):
 
     def to_mongo(self, value):
         return value and self._encrypt(value) or None
+
+
+class EncryptedStringField(EncryptedBinaryField):
+    """
+    Encrypted Unicode string field. Encryption is completely transparent
+    to the caller as the field automatically decrypts when the field
+    is accessed and encrypts when the document is saved. The
+    underlying algorithm currently is AES-256.
+    """
+
+    def to_python(self, value):
+        return value and self._decrypt(value).decode('utf-8') or None
+
+    def to_mongo(self, value):
+        return value and self._encrypt(value.encode('utf-8')) or None
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,9 @@ if sys.version_info[0] > 2:
         'test_legacy.py',
         'test_declenum.py',
         'test_formfields.py',
-        'mongo',
+        'mongo/test_documents.py',
+        'mongo/test_utils.py',
+        'mongo/fields/test_basic.py',
+        'mongo/fields/test_phone.py',
+        'mongo/fields/test_tz.py',
     ]
